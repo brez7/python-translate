3b76263da84a9cbccd8048df6c406b5a4f5721eb
brez7 <rob@barbank.com>
2024-02-28 13:05:44 -0800

    prepend downloaded file name

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import chardet  # Import chardet
 
 app = Flask(__name__)
 translate_client = translate.Client()
-# hello
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -14,20 +14,27 @@ def index():
         if file:
             raw_data = file.stream.read()
             result = chardet.detect(raw_data)
-            encoding = result['encoding'] if result['encoding'] is not None else 'utf-8'  # Default to utf-8 if chardet returns None
-            po = polib.pofile(raw_data.decode(encoding))  # Use detected or default encoding
+            encoding = result['encoding'] if result['encoding'] is not None else 'utf-8'
+            po = polib.pofile(raw_data.decode(encoding))
             for entry in po.untranslated_entries():
                 result = translate_client.translate(entry.msgid, target_language='es')
                 entry.msgstr = result['translatedText']
             buffer = io.BytesIO()
             buffer.write(str(po).encode('utf-8'))
             buffer.seek(0)
+
+            # Extract the first 10 characters of the file's name for the download name
+            original_filename = file.filename
+            download_filename = "Translated-" + (original_filename[:10] + '.po' if len(original_filename) > 10 else original_filename)
+
+            buffer.seek(0)
             return send_file(
                 buffer,
                 as_attachment=True,
-                download_name='translated.po',
+                download_name=download_filename,  # Use the updated variable here
                 mimetype='application/octet-stream'
             )
+
     return '''<!doctype html>
 <html lang="en">
 <head>
@@ -37,11 +44,11 @@ def index():
             color: #fff; 
             background-color: #333; 
             font-family: Arial, sans-serif; 
-            margin: 20px 100px; /* Adjust margin as needed */
+            margin: 20px 100px;
             padding: 0; 
         }
         h1 { 
-            color: #0d6efd; /* Bootstrap primary blue for a bit of contrast */
+            color: #0d6efd;
         }
         form { 
             margin-top: 20px; 
@@ -56,10 +63,10 @@ def index():
             background-color: #0d6efd; 
             color: white; 
             border: none; 
-            padding: 10px 20px; /* Larger padding */
+            padding: 10px 20px;
             margin-top: 10px; 
             cursor: pointer; 
-            font-size: 16px; /* Larger font size */
+            font-size: 16px;
         }
         input[type="submit"]:hover { 
             background-color: #0b5ed7; 
